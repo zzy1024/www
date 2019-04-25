313822e811708d21107ba22e7afcd05b607cd42e
zhuzhongya <18538758620@139.com>
2019-04-25 17:42:20 +0800

    zzy

--- test1.php
+++ test1.php
@@ -9,4 +9,32 @@ EOF;
 echo $eof;
 $cars = array("Volvo","BMW","Toyota");
 var_dump($cars);
+
+define('demoTitle', 'learnPhpDemo');
+echo demoTitle;
+
+$txt = 'hello world!';
+$txt[0] = 'm';
+echo '<p>'.$txt.'长度是：'.strlen($txt).',wor字符所在的位置：'.strpos($txt, 'wor').'</p>';
+echo date('H');
+
+$arr = array('101', '23', '2222');
+var_dump($arr);
+echo '$arr数组长度by count：', count($arr), '<br/>';
+/*for($i = 0; $i < count($arr); $i++){
+    echo $i;
+    echo '<br/>';
+}*/
+sort($arr);
+print_r($arr);
+
+/*$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+foreach($age as $x=>$x_value)
+{
+    echo "Key=" . $x . ", Value=" . $x_value;
+    echo "<br>";
+}*/
+
+var_dump($GLOBALS);
+
 ?>
