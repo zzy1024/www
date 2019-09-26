98272770c7a3c4650ce16f3a9b9bc5a1b4723fd4
zhuzhongya <18538758620@139.com>
2019-09-26 17:56:19 +0800

    php

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,7 +1,14 @@
 <?php
     namespace app\index\controller;
-    class Index{
+    use think\Controller;
+
+    class Index extends Controller{
         public function index(){
-            return 'this is a app\index\controller';
+            //return 'this is a app\index\controller';
+            //return view('./index.html');
+            return $this->fetch('./index.html', [
+                'email' => '890@qq.com',
+                'userName' => 'zzy',
+            ]);
         }
     }
