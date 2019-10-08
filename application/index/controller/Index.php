bb82ef5b7b9c51deb1364fb893eafe22b8ad94c8
zhuzhongya <18538758620@139.com>
2019-10-08 16:56:50 +0800

    tijao

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,14 +1,19 @@
 <?php
-    namespace app\index\controller;
-    use think\Controller;
+namespace app\index\controller;
+use think\Controller;
+use think\Db;
+use think\config;
 
-    class Index extends Controller{
-        public function index(){
-            //return 'this is a app\index\controller';
-            //return view('./index.html');
-            return $this->fetch('./index.html', [
-                'email' => '890@qq.com',
-                'userName' => 'zzy',
-            ]);
-        }
+class Index extends Controller
+{
+    public function index()
+    {
+        //dump(Db::connect());
+        $result = Db::query('select * from imooc_user where id = 1');
+        dump($result);
+        //$result1 = Db::execute('insert into imooc_user (username, password ,email) values (?, ?, ?)',["dawei2", "thinkphp2","think2@qq.com"]);
+        //Db::table('imooc_user')->where('id',4)->delete();
+        dump(Db::query('select * from imooc_user'));
+        return '123';
     }
+}
---
+++ conf/config.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'SHOW_PAGE_TRACE'=>true,
+    'app_debug'              => true,
+];
